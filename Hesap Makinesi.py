e96c8b64b00427a76222a83e8b40623ac0c7262c
12acar21 <166230866+12acar21@users.noreply.github.com>
2024-04-12 19:56:56 +0300

    Hesap Makinesi v1.6.py

--- Hesap Makinesi.py
+++ Hesap Makinesi.py
@@ -1,49 +1,64 @@
+#hesap makinesi v1.6
+
+print(""">Yüzde hesaplamak için \"%\" yazınız.
+>Üs almak için işlem bölümüne \"**\" yazınız.""")
+
 while True:
-    sayi1 = input("1.sayı:")
+    sayi1= input("1.Sayı:")
+    if sayi1== "%":
+        x = int(input("Sayı:"))
+        y = int(input("%"))
+        z = y / 100
+        sonuc = x * z
+        print("Sonuç:", sonuc)
+        break
+    elif not sayi1.isdigit():
+        print("Lütfen geçerli bir tam sayı giriniz!")
+        continue
+    else:
+        sayi1= int(sayi1)
+        break
 
-    if not sayi1.isdigit():
-        print("Lütfen sayıyı doğru giriniz!")
-
-
-islem= input("İşlem:")
 while True:
-    if islem=="+":
+    if sayi1=="%":
         break
-    elif islem=="-":
-        break
-    elif islem=="*":
-        break
-    elif islem=="/":
+    islem= input("İşlem:")
+    if islem in ["+", "-", "*", "/", "**"]:
         break
     else:
-        if islem != "+":
-            if islem != "-":
-                if islem != "*":
-                    if islem != "/":
-                        print("!!!Lütfen işlem bölümüne + , - , * , /  karakterlerini giriniz!!!")
-                        islem= input("İşlem:")
-                        continue
+        print("Lütfen geçerli bir işlem giriniz! ( + , - , * , / , ** )")
+        continue
 
 while True:
-    sayi2= input("2.sayı:")
-
+    if sayi1=="%":
+        break
+    sayi2= input("2.Sayı:")
     if not sayi2.isdigit():
-        print("Lütfen sayıyı doğru giriniz!")
-
-
-
-sayi1, sayi2= int(sayi1), int(sayi2)
-
-if islem=="+":
-    print(sayi1+sayi2)
-elif islem=="-":
-    print(sayi1-sayi2)
-elif islem=="*":
-    print(sayi1*sayi2)
-elif islem=="/":
-    if sayi2 == 0:
-        print("Tanımsız! (Lütfen sayınızı 0 haricinde bir sayıya bölünüz!)")
+        print("Lütfen geçerli bir tam sayı giriniz!")
+        continue
     else:
-        print(sayi1 / sayi2)
+        sayi2= int(sayi2)
+        break
 
+while True:
+    if sayi1=="%":
+        break
+    else:
+        if islem == "+":
+            sonuc = sayi1 + sayi2
+        elif islem == "-":
+            sonuc = sayi1 - sayi2
+        elif islem == "*":
+            sonuc = sayi1 * sayi2
+        elif islem == "/":
+            sonuc = sayi1 / sayi2
+        elif islem == "**":
+            sonuc = sayi1 ** sayi2
+        break
 
+while True:
+    if sayi1=="%":
+        break
+    else:
+        print(sonuc)
+        break
